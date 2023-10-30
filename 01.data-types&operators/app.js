76ee9332498f55981fb9861b4a0db3b3353dd310
ZeynalovaAziza <akulaalekb@gmail.com>
2023-10-30 08:58:37 -0700

    class

---
+++ 01.data-types&operators/app.js
@@ -0,0 +1,56 @@
+//task1
+//part1
+let firstName = "gulnur";
+let lastName = "esgerova";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
+//part2
+let country = "Azerbaijan";
+let age = "19";
+let job = "study";
+
+console.log( fullName +" . I am a " +job + ". I live in "  + country+" . I 10 years old ") ;
+
+//task 2
+//part1
+let length = 16 ; //number
+
+console.log(typeof length)
+let Color = " Brown" ; //string
+console.log(typeof Color)
+let float = "12.5" ; //string
+console.log(typeof float)
+let like = true //boolean
+console.log(typeof like)
+let x = {
+    firstName: "Nick" ,
+    Color: "Red " ,
+} ; //undefined
+console.log(typeof lx)
+
+// task3
+
+let distance = 8600 ;
+let speed = 120 ;
+let duration = distance / speed ;
+console.log (
+   "  The car will arrive in china in " + duration + " minutes"
+);
+ //task4
+ let year = 2023;
+ let birth = 2004;
+ let minus =  year - birth ;
+ console.log(minus + " " + (minus+1));
+
+ //task5 
+ let leman =  27;
+ let sema = 34;
+ let almaz = 41;
+ let gulus = 52;
+ let sum = (leman + sema + almaz + gulus )/4 ;
+
+ console.log(sum);
+
+ 
