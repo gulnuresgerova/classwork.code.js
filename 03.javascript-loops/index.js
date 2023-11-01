a83bf5cf41ed0304a3f19e97499a36804f63ddb0
ZeynalovaAziza <akulaalekb@gmail.com>
2023-11-01 08:46:17 -0700

    class

---
+++ 03.javascript-loops/index.js
@@ -0,0 +1,58 @@
+//task1
+// for( let i=0 ; i < 100 ; i++){
+// if(i% 2 === 0){
+//     console.log( `${i}`)
+// }
+// }
+//task2
+// let str = "gulnur";
+// console.log(str[str.length - 1]);
+// console.log(str.length);
+
+// for (i = 0; i < str.length; i++) {
+//   console.log(str[i]);
+// }
+//task3
+// for(let i =1 ; i<100 ; i++){
+//     if(i % 10 === 7){
+//         console.log( `${i}`);
+//     }
+// }
+//task4
+// for(let i =10 ; i<100 ; i++){
+//     if(i % 11 === 0){
+//         console.log( `${i}`);
+//         }
+//     }
+//task5
+// let a = 40;
+// for (let i = 0; i < 100; i++) {
+//   if (a % i === 0) {
+//     console.log(`${i}`);
+//   }
+// }
+// //task6
+// let number=7;
+// let factorial =1;
+// for ( let i=1 ; i<=number ; i++ ){
+//     factorial *=i ;
+   
+// }
+// console.log( `${factorial}`)
+// //task7
+// //let sum =0;
+// for ( let i =2 ; i <= 10 ; i++){
+//     if( i%2=== 0){
+//         sum += i;
+//     }
+//     console.log( sum);
+// }
+//task8
+let sum =0;
+for (let i=11 ; i< 50 ; i++){
+    if (i %3 ===0) {
+        sum = sum +i;
+    }
+}
+        console.log( sum);
+  
