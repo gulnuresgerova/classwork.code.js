1fa797866470c2ee70618288ae4b1fb04dd7c1ef
ZeynalovaAziza <akulaalekb@gmail.com>
2023-11-03 09:08:17 -0700

    c

---
+++ 04.array&object/index.js
@@ -0,0 +1,45 @@
+//task1
+
+// let numbers = [12, 45, 6, 78, 34, 99, 23];
+// console.log(Math.max(...numbers));
+// console.log(Math.min(...numbers));
+
+//task2
+// let sum = 0;
+// let numbers = [12, 45, 6, 78, 34, 99, 23];
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+// console.log(sum);
+
+//task3
+
+// let elements = [ 1 , 2 , 3 , 4 , 5];
+// elements[1]=3;
+// elements[2]=2;
+// console.log( elements)
+
+
+//task4
+// let numbers = [ 12 , -5 , 6 , -3 , 34 , -1 ,23];
+// let posotiveNumbers = [12 , 6 , 34 , 23];
+
+// for ( let i =0 ; i < posotiveNumbers ; i++ );
+// {
+//     console.log(posotiveNumbers)
+// }
+
+
+
+//task5 
+// let originalArray = [1 , 2 , 3 ,4];
+//  let repeatedArray = [];
+//  let n =3 ;
+
+//  for ( let i =0 ;  i< originalArray.length; i++){
+//     for(  let a =0 ; a<n ; a++){
+// repeatedArray.push( originalArray[i]) 
+//     }
+//  }
+//  console.log(  repeatedArray)
+
