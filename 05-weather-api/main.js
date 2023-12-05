9cf9cd259ee895e06a8f02da0fa166f9379ea41c
ZeynalovaAziza <akulaalekb@gmail.com>
2023-12-05 06:14:15 -0800

    class

---
+++ 05-weather-api/main.js
@@ -0,0 +1,20 @@
+let box = document.querySelector(".box");
+let container = document.querySelector(".container")
+let forecast = document.querySelector(".forecast")
+let img = document.createElement("img"); 
+let btn = document.querySelector(".btn")
+let input = document.querySelector(".input")
+
+// BASE_URL=(`https://api.weatherapi.com/v1/current.json?key=de4fcc1d06a64857be7101727222212&q=$`)
+
+forecast.append(img);
+
+setInterval(() => {
+//   fetch(`https://api.weatherapi.com/v1/current.json?key=de4fcc1d06a64857be7101727222212&q=$`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+
+
+    });
+});
